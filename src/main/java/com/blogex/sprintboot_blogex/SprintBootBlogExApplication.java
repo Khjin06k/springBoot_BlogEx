ac482da0bf4f5d5ec846bb734a024f5f9a0b7794
gmlwls <khj4209kk@gmail.com>
2023-09-07 08:56:25 +0900

    시작 전 세팅

---
+++ src/main/java/com/blogex/sprintboot_blogex/SprintBootBlogExApplication.java
@@ -0,0 +1,13 @@
+package com.blogex.sprintboot_blogex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootBlogExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintBootBlogExApplication.class, args);
+    }
+
+}
